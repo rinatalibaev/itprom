bcca924303fc2d5d881d77ae7d2b65d87df1af61
rinat.alibaev@gmail.com <rinat.alibaev@gmail.com>
2019-12-10 08:16:34 +0500

    + Устранение замечаний + Тесты

---
+++ src/main/java/ru/alibaev/itprom/controller/MainController.java
@@ -0,0 +1,15 @@
+package ru.alibaev.itprom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String get() {
+        return "redirect:/profession/getAll";
+    }
+}
--- src/test/java/ru/alibaev/itprom/ItpromApplicationTests.java
+++ src/test/java/ru/alibaev/itprom/ItpromApplicationTests.java
@@ -1,13 +1,53 @@
 package ru.alibaev.itprom;
 
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ItpromApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	ItpromApplicationTests() {
+	}
+
 	@Test
-	void contextLoads() {
+	void testProfessionCreate (){
+		String url = "http://localhost:" + port + "/profession/create";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("name", "Svarschik");
+		jsonObject.put("note", "Some profession");
+		HttpEntity<String> request =
+				new HttpEntity<>(jsonObject.toString(), headers);
+		ResponseEntity<Void> responseEntity = restTemplate.postForEntity(url, request, Void.class);
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+	}
+
+	@Test
+	void testDepartmentCreate (){
+		String url = "http://localhost:" + port + "/department/create";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("name", "RZA");
+		jsonObject.put("note", "Some dep");
+		HttpEntity<String> request =
+				new HttpEntity<>(jsonObject.toString(), headers);
+		ResponseEntity<Void> responseEntity = restTemplate.postForEntity(url, request, Void.class);
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 	}
 
 }
